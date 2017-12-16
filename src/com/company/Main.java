5e37105e971c625a8a37f14080325ecae35bed10
hhlTer <grzc@ukr.net>
2017-12-16 23:29:07 +0200

    HomeWork_3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,17 +14,19 @@ public class Main {
 //        for (int i = 0; i < count; i++) {
 //            arrayInt[i] = getIntChecking();
 //        }
-        int[] arrayInt = {5, 6, -4, 1, 11, -9, 0};
+        int[] arrayInt = {5, 6, -4, 1, 11, -9, 1, 0, 1, 5};
         System.out.printf("Minimum digit: %d\n", getMinimumFromArray(arrayInt));
         System.out.printf("Maximum digit: %d\n", getMaximumFromArray(arrayInt));
         System.out.printf("Count of repeats number 5: %d\n", countOfNumber(arrayInt, 5));
+        System.out.println("Sort:");
         for (int a:
              sort(arrayInt)) {
             System.out.print((a) + " ");
         }
-
-
+        System.out.println("\nMaximum count same of digits: " + countOfRepeat(arrayInt));
+        System.out.println("Minimum count same of digits: " + countMinOfRepeat(arrayInt));
     }
+
     private static int countOfNumber(int[] array, int d){
         int count = 0;
         for (int anArray : array) {
@@ -79,6 +81,30 @@ public class Main {
             }
         }
         return array;
+    }
+
+    private static int countOfRepeat(int[] array){
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            int temp = 0;
+            for (int j = i; j < array.length; j++) {
+                if (array[i] == array[j]) temp++;
+            }
+            if (temp > count) count = temp;
+        }
+        return count;
+    }
+
+    private static int countMinOfRepeat(int[] array){
+        int count = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            int temp = 0;
+            for (int j = i; j < array.length; j++) {
+                if (array[i] == array[j]) temp++;
+            }
+            if (temp < count & temp > 1) count = temp;
+        }
+        return count;
     }
     
 }
